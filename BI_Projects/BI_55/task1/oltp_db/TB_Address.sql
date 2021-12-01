6299aafa337779ef81901522972d59826cf3aa75
Arianna Pera <54574943+ariannap13@users.noreply.github.com>
2021-12-01 18:48:11 +0100

    Update TB_Address.sql

--- BI_Projects/BI_55/task1/oltp_db/TB_Address.sql
+++ BI_Projects/BI_55/task1/oltp_db/TB_Address.sql
@@ -3,7 +3,7 @@ CREATE TABLE TB_Address (
     CountryID INT NOT NULL,
     AddressLine VARCHAR(60) NOT NULL,
     City VARCHAR(30) NOT NULL,
-    PostalCode VARCHAR(30) NOT NULL,
+    PostalCode VARCHAR(15) NOT NULL,
     CONSTRAINT PK_Address PRIMARY KEY (AddressID), 
     CONSTRAINT FK_Country_Address FOREIGN KEY (CountryID)  
     REFERENCES Country(CountryID)  
