fae875fa4ec9f33ea5184b1c5f9606fb5ce63f47
Arianna Pera <54574943+ariannap13@users.noreply.github.com>
2021-12-04 18:39:33 +0100

    Update Dim_Customer.sql

--- BI_Projects/BI_55/task2/Dim_Customer.sql
+++ BI_Projects/BI_55/task2/Dim_Customer.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE Dim_Customer (
+    CustomerKey INT NOT NULL,
+    AccountNumber VARCHAR(30) NOT NULL,
+    FullName VARCHAR(30) NOT NULL,
+    Gender VARCHAR(1),
+	Age INT,
+    CONSTRAINT PK_DimCustomer PRIMARY KEY (CustomerKey)
+);  
