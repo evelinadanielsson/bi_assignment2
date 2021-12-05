a5b9c0fffba01947ba07c7b130be883d2e4d27bc
Arianna Pera <54574943+ariannap13@users.noreply.github.com>
2021-12-05 18:41:36 +0100

    fullname definition was wrong

--- BI_Projects/BI_55/task2/data_mart/Dim_Customer.sql
+++ BI_Projects/BI_55/task2/data_mart/Dim_Customer.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Dim_Customer (
     CustomerKey INT NOT NULL,
     AccountNumber VARCHAR(30) NOT NULL,
-    FullName VARCHAR(30) NOT NULL,
+    FullName VARCHAR(150) NOT NULL,
     Gender VARCHAR(1),
 	Age INT,
     CONSTRAINT PK_DimCustomer PRIMARY KEY (CustomerKey)
